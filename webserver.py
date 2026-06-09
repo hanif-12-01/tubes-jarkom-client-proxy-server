d9eb25c5f989a36ed07a6c06921d424f3eb6db55
Auto User <auto@example.com>
2026-06-09 16:57:51 +0700

    yang lama JARKOM mlzzzz

--- webserver.py
+++ webserver.py
@@ -5,8 +5,9 @@ import time
 import mimetypes
 from datetime import datetime
 
-# Menggunakan IP terpisah untuk simulasi topologi jaringan
-SERVER_IP = '10.130.49.153'
+# Menggunakan IP terpisah untuk Web Server dan Proxy agar bisa diuji firewall socket
+SERVER_IP = '10.130.49.153'  # IP Laptop Web Server (WAJIB GANTI)
+ALLOWED_PROXY_IP = '10.130.49.136' # WAJIB GANTI: Isi dengan IP Laptop Proxy
 HTTP_PORT = 8000
 UDP_PORT = 9000
 
